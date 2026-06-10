a003d097990a2fcd8969ea7d2421f0c059db8765
Gabriela Alves de Oliveira <agaby213@gmail.com>
2026-06-10 10:52:17 -0300

    Exemplos básicos utilizando o While.

---
+++ #While - Enquanto.py
@@ -0,0 +1,25 @@
+#While - Enquanto
+#While - loops
+
+numero = 1
+while numero < 5:
+    print(numero)
+
+    numero += 1 
+
+    contador = 10
+
+    while contador >= 1:
+
+        print(contador)
+
+        contador -=1
+
+
+contador = 10
+while contador >= 1:
+
+   print(contador)
+
+   contador -=2
+

2889bbc77814cc6264e6725262983f36d5c68f7c
Gabriela Alves de Oliveira <agaby213@gmail.com>
2026-06-12 08:37:56 -0300

    Exemplo básico utilizando condição max.

---
+++ While, max.py
@@ -0,0 +1,14 @@
+numero = 1
+
+max = int(input("Digite um inteiro maior que 1: "))
+
+print("Números pares entre 1 e", max, ":")
+            
+while numero <= max:
+            
+            if numero % 2 == 0:
+            
+             print(numero, end=" ")
+
+            numero += 1
+
